bbcefb2e4eddfb4eee562247c661b3172e6ff8a2
FridaMarRamirez <frida.mar.ramirez@gmail.com>
2024-10-24 09:58:06 -0400

    update javascript

--- script.js
+++ script.js
@@ -8,28 +8,53 @@ let brown_position = 1;
 
 
 // Query Selectors Here
+// blue horse 
 const blueButton = document.querySelector("#blue-button");
 const blueHorse = document.querySelector("#blue-horse");
-
+// pink horse
+const pinkButton = document.querySelector("#pink-button");
+const pinkHorse = document.querySelector("#pink-horse");
+// brown horse
+const brownButton = document.querySelector("#brown-button");
+const brownHorse = document.querySelector("#brown-horse");
+// defining the winner
+const horseWinner = document.querySelector("#winner");
 
 
 // OnClick Functions Here
 const advanceBlue = (e) => {
   blue_position += 1;
   changePosition(blueHorse, blue_position);
+  return checkWinner();
 };
 const advancePink = (e) => {
-
+  pink_position += 1;
+  changePosition(pinkHorse, pink_position)
+  return checkWinner();
 };
-
+const advanceBrown = (e) => {
+  brown_position += 1;
+  changePosition(brownHorse, brown_position);
+  return checkWinner();
+}
 
 
 // Check for a winner
 const checkWinner = (position, color) => {
-
+  if (blue_position == 5) {
+    horseWinner.innerHTML = '<p>Blue is the Winner!</p>';
+  }
+  else if (pink_position == 5) {
+    horseWinner.innerHTML = '<p>Pink is the Winner!</p>';
+  }
+  else if (brown_position == 5) {
+    horseWinner.innerHTML = '<p>Brown is the Winner!</p>';
+  }
 };
 
 
 
 // Event Listeners Here
 blueButton.addEventListener("click", advanceBlue);
+pinkButton.addEventListener("click", advancePink);
+brownButton.addEventListener("click", advanceBrown);
